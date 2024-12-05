28ee2071b246b19b8c8857e8bdf62d89442e3887
ebruaeo <ebruaeo@hotmail.com>
2024-12-05 15:19:00 +0300

    Adding a Floating Action Button

--- app/src/main/java/com/example/designingtools/home/HomeFragment.kt
+++ app/src/main/java/com/example/designingtools/home/HomeFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.designingtools.R
+import com.example.designingtools.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+lateinit var binding : FragmentHomeBinding
     companion object {
         fun newInstance() = HomeFragment()
     }
@@ -20,9 +23,21 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+         binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
 
-        return view
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { navigate ->
+            if (navigate){
+                val navController = findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
     }
 }
